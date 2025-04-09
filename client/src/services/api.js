7588cd74b49e61c0677531ae85eee3c8b961dcf5
sudhan1112 <sudhanssudhan83@gmail.com>
2025-04-09 07:54:42 +0530

    Deployment completed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://recipe-management-system-hkt4.onrender.com/api'
 });
 
 export const getRecipes = async () => {
